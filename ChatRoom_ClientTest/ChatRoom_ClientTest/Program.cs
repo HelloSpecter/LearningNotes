34e494cb02398f3a0a59e47333af5cc906147292
HelloSpecter <281141569@qq.com>
2022-07-11 16:44:11 +0800

    -update 客户端多线程接收消息
    -update 客户端多线程接收消息

--- ChatRoom_ClientTest/ChatRoom_ClientTest/Program.cs
+++ ChatRoom_ClientTest/ChatRoom_ClientTest/Program.cs
@@ -5,25 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace ChatRoom_ClientTest
 {
     class Program
     {
         public static byte[] data_recieve = new byte[1024];
+        public static TcpClient client = new TcpClient("127.0.0.1", 7788);
         static void Main(string[] args)
         {
-            
-            TcpClient client = new TcpClient("127.0.0.1", 7788);
             NetworkStream stream = client.GetStream();
-
+            Thread thread;
+            thread = new Thread(ReceiveMessage);
+            thread.Start();
             while (true)
             {
                 Console.WriteLine("请输入发给服务器的数据内容：\n");
                 string message = Console.ReadLine();
                 byte[] data = Encoding.UTF8.GetBytes(message);
                 stream.Write(data, 0, data.Length);
-                ReceiveMessage(client.Client);
             }
 
             stream.Close();
@@ -31,13 +32,13 @@ namespace ChatRoom_ClientTest
             Console.ReadKey();
         }
         
-        private static void ReceiveMessage(Socket client)
+        private static void ReceiveMessage()
         {
-            int msg_Len = client.Receive(data_recieve);
-            if (msg_Len > 0)
+            while (true)
             {
+                int msg_Len = client.Client.Receive(data_recieve);
                 string msg = Encoding.UTF8.GetString(data_recieve, 0, msg_Len);
-                Console.WriteLine("Client接受到的消息是：\n" + msg);
+                Console.WriteLine(string.Format("[{0}]接受到的消息是：\n {1}", DateTime.Now, msg));
             }
         }
     }
